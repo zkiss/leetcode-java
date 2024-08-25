bd651956ed33fc628aa22a574ecd8836f44a57c2
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2024-08-25 20:55:08 +0100

    https://leetcode.com/problems/dota2-senate

--- src/main/java/net/zkiss/leetcode/Dota2Senate.java
+++ src/main/java/net/zkiss/leetcode/Dota2Senate.java
@@ -3,64 +3,35 @@ package net.zkiss.leetcode;
 //https://leetcode.com/problems/dota2-senate
 public class Dota2Senate {
     public String predictPartyVictory(String senate) {
-        if (senate.length() == 1) {
-            return result(senate.charAt(0));
-        }
-
         var ss = senate.toCharArray();
-        var k = 0;
+        var balance = 0; // d <= 0 <= r
         while (true) {
-            int target = next(enemyOf(ss[k]), ss, k);
-            if (target == -1) {
-                return result(ss[k]);
+            var rLive = 0;
+            var dLive = 0;
+            for (int i = 0; i < ss.length; i++) {
+                if (ss[i] == 'R') {
+                    if (balance < 0) {
+                        ss[i] = 'X';
+                    } else {
+                        rLive++;
+                    }
+                    balance++;
+                } else if (ss[i] == 'D') {
+                    if (0 < balance) {
+                        ss[i] = 'X';
+                    } else {
+                        dLive++;
+                    }
+                    balance--;
+                }
             }
-            ss[target] = 'X';
-            k = next(ss, k);
+            if (rLive == 0) {
+                return "Dire";
+            }
+            if (dLive == 0) {
+                return "Radiant";
+            }
         }
     }
 
-    private int next(final char search, char[] senate, final int start) {
-        var i = start;
-        while (senate[i] != search) {
-            i++;
-            if (i >= senate.length) {
-                i = 0;
-            }
-            if (i == start) {
-                return -1;
-            }
-        }
-        return i;
-    }
-
-    private char enemyOf(char s) {
-        return switch (s) {
-            case 'D' -> 'R';
-            case 'R' -> 'D';
-            default -> throw new IllegalArgumentException("" + s);
-        };
-    }
-
-    private String result(char c) {
-        return switch (c) {
-            case 'D' -> "Dire";
-            case 'R' -> "Radiant";
-            default -> throw new IllegalArgumentException("" + c);
-        };
-    }
-
-
-    private int next(char[] senate, final int start) {
-        if (senate[start] == 'X') {
-            throw new IllegalArgumentException("start");
-        }
-        int next = start;
-        do {
-            next++;
-            if (next >= senate.length) {
-                next = 0;
-            }
-        } while (senate[next] == 'X');
-        return next;
-    }
 }
