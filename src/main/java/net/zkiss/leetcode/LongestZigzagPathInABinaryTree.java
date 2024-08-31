5a55d23333746c34b9016b725ffd7e05fcfd85b2
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2024-08-31 21:31:48 +0100

    https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree

--- src/main/java/net/zkiss/leetcode/LongestZigzagPathInABinaryTree.java
+++ src/main/java/net/zkiss/leetcode/LongestZigzagPathInABinaryTree.java
@@ -2,37 +2,21 @@ package net.zkiss.leetcode;
 
 import net.zkiss.leetcode.common.TreeNode;
 
-import java.util.IdentityHashMap;
-import java.util.Map;
-
 //https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree
 public class LongestZigzagPathInABinaryTree {
     public int longestZigZag(TreeNode root) {
-        var paths = new IdentityHashMap<TreeNode, int[]>();
-        return dfs(root, paths, 0);
+        return dfs(root, 0, 0, 0);
     }
 
-    private int dfs(TreeNode root, Map<TreeNode, int[]> paths, int max) {
+    private int dfs(TreeNode root, int left, int right, int max) {
         if (root == null) {
             return 0;
         }
-        if (root.left == null && root.right == null) {
-            paths.put(root, new int[]{0, 0});
-            return max;
-        }
-        max = Math.max(max, dfs(root.left, paths, max));
-        max = Math.max(max, dfs(root.right, paths, max));
+        max = Math.max(max, Math.max(left, right));
 
-        var left = 0;
-        var right = 0;
-        if (root.left != null) {
-            left = 1 + paths.get(root.left)[1];
-        }
-        if (root.right != null) {
-            right = 1 + paths.get(root.right)[0];
-        }
-        paths.put(root, new int[]{left, right});
+        max = Math.max(max, dfs(root.left, right + 1, 0, max));
+        max = Math.max(max, dfs(root.right, 0, left + 1, max));
 
-        return Math.max(max, Math.max(left, right));
+        return max;
     }
 }
