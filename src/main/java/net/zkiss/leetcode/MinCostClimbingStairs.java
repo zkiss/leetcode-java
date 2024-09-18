dd8287676e724bbd4d5f25770247f61f1af2deff
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2024-09-18 17:52:33 +0100

    https://leetcode.com/problems/min-cost-climbing-stairs

--- src/main/java/net/zkiss/leetcode/MinCostClimbingStairs.java
+++ src/main/java/net/zkiss/leetcode/MinCostClimbingStairs.java
@@ -3,21 +3,9 @@ package net.zkiss.leetcode;
 //https://leetcode.com/problems/min-cost-climbing-stairs
 public class MinCostClimbingStairs {
     public int minCostClimbingStairs(int[] cost) {
-        var res = new Integer[cost.length];
-        res[cost.length - 1] = cost[cost.length - 1];
-        res[cost.length - 2] = cost[cost.length - 2];
-        return Math.min(
-                dp(0, cost, res),
-                dp(1, cost, res));
-    }
-
-    private int dp(int i, int[] cost, Integer[] res) {
-        if (res[i] != null) {
-            return res[i];
+        for (int i = cost.length - 3; i >= 0; i--) {
+            cost[i] = cost[i] + Math.min(cost[i + 1], cost[i + 2]);
         }
-        res[i] = cost[i] + Math.min(
-                dp(i + 1, cost, res),
-                dp(i + 2, cost, res));
-        return res[i];
+        return Math.min(cost[0], cost[1]);
     }
 }
