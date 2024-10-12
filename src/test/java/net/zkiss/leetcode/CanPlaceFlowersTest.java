86b16145ec5d4d316a96bdbc505acc403239245b
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2024-10-12 14:57:12 +0100

    https://leetcode.com/problems/can-place-flowers

---
+++ src/test/java/net/zkiss/leetcode/CanPlaceFlowersTest.java
@@ -0,0 +1,34 @@
+package net.zkiss.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CanPlaceFlowersTest {
+
+    private final CanPlaceFlowers cpf = new CanPlaceFlowers();
+
+    @Test
+    void example1() {
+        assertThat(cpf.canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 1)).isTrue();
+    }
+
+    @Test
+    void example2() {
+        assertThat(cpf.canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 2)).isFalse();
+    }
+
+    @Test
+    void one() {
+        assertThat(cpf.canPlaceFlowers(new int[]{0}, 1)).isTrue();
+        assertThat(cpf.canPlaceFlowers(new int[]{1}, 1)).isFalse();
+    }
+
+    @Test
+    void two() {
+        assertThat(cpf.canPlaceFlowers(new int[]{0, 0}, 1)).isTrue();
+        assertThat(cpf.canPlaceFlowers(new int[]{0, 0}, 2)).isFalse();
+        assertThat(cpf.canPlaceFlowers(new int[]{1, 0}, 1)).isFalse();
+        assertThat(cpf.canPlaceFlowers(new int[]{0, 1}, 1)).isFalse();
+    }
+}
