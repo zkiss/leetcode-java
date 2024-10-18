c1d2c02bbc3a3e1c9944d8deb80f48269d2d3fc4
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2024-10-18 21:55:00 +0100

    https://leetcode.com/problems/decode-string

--- src/main/java/net/zkiss/leetcode/DecodeString.java
+++ src/main/java/net/zkiss/leetcode/DecodeString.java
@@ -4,49 +4,32 @@ package net.zkiss.leetcode;
 public class DecodeString {
     public String decodeString(String s) {
         var sb = new StringBuilder();
-        for (int i = 0; i < s.length(); ) {
-            char c = s.charAt(i);
+        int i = 0;
+        while (i < s.length()) {
+            var c = s.charAt(i);
+
             if (Character.isAlphabetic(c)) {
                 sb.append(c);
                 i++;
                 continue;
             }
 
-            if (!Character.isDigit(c)) {
-                throw new RuntimeException("digit expected");
+            var start = i;
+            while (Character.isDigit(s.charAt(i))) {
+                i++;
             }
+            var multiplier = Integer.parseInt(s.substring(start, i));
 
-            var j = i + 1;
-            while (Character.isDigit(s.charAt(j))) {
-                j++;
-            }
-            var multiplier = Integer.parseInt(s.substring(i, j));
-
-            if ('[' != s.charAt(j)) {
-                throw new RuntimeException("'[' expected");
-            }
-
-            int balance = 1;
-            var ss = j + 1;
+            var balance = 1;
+            start = ++i;
             while (balance > 0) {
-                switch (s.charAt(ss)) {
-                    case '[': {
-                        balance++;
-                        break;
-                    }
-                    case ']': {
-                        balance--;
-                        break;
-                    }
-                }
-                ss++;
+                var cc = s.charAt(i++);
+                if (cc == '[') balance++;
+                if (cc == ']') balance--;
             }
 
-            var multiplied = decodeString(s.substring(j + 1, ss - 1));
-            i = ss;
-            sb.repeat(multiplied, multiplier);
+            sb.repeat(decodeString(s.substring(start, i - 1)), multiplier);
         }
-
         return sb.toString();
     }
 }
