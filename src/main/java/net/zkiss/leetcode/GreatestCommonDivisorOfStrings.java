910fce2ee4d8f1962bb054d7e4c56e3410829e0e
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2025-01-26 17:58:35 +0000

    https://leetcode.com/problems/greatest-common-divisor-of-strings

--- src/main/java/net/zkiss/leetcode/GreatestCommonDivisorOfStrings.java
+++ src/main/java/net/zkiss/leetcode/GreatestCommonDivisorOfStrings.java
@@ -14,13 +14,8 @@ public class GreatestCommonDivisorOfStrings {
     }
 
     private int gcd(int a, int b) {
-        if (b > a) {
-            return gcd(b, a);
-        }
-        var ab = a % b;
-        if (ab == 0) {
-            return b;
-        }
-        return gcd(b, ab);
+        if (b > a) return gcd(b, a);
+        if (b == 0) return a;
+        return gcd(b, a % b);
     }
 }
