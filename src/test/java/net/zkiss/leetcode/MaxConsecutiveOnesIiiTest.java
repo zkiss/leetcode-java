4c74889f7facc725c1ca61c936c0f9981c7c04fa
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2025-01-29 16:45:35 +0000

    https://leetcode.com/problems/max-consecutive-ones-iii

--- src/test/java/net/zkiss/leetcode/MaxConsecutiveOnesIiiTest.java
+++ src/test/java/net/zkiss/leetcode/MaxConsecutiveOnesIiiTest.java
@@ -17,4 +17,23 @@ class MaxConsecutiveOnesIiiTest {
     void example2() {
         assertThat(m.longestOnes(new int[]{0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1}, 3)).isEqualTo(10);
     }
+
+    @Test
+    void no_flip() {
+        assertThat(m.longestOnes(new int[]{0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0}, 0)).isEqualTo(5);
+    }
+
+    @Test
+    void no_ones() {
+        assertThat(m.longestOnes(new int[]{0, 0, 0, 0, 0, 0}, 0)).isEqualTo(0);
+        assertThat(m.longestOnes(new int[]{0, 0, 0, 0, 0, 0}, 1)).isEqualTo(1);
+        assertThat(m.longestOnes(new int[]{0, 0, 0, 0, 0, 0}, 2)).isEqualTo(2);
+        assertThat(m.longestOnes(new int[]{0, 0, 0, 0, 0, 0}, 100)).isEqualTo(6);
+    }
+
+    @Test
+    void all_ones() {
+        assertThat(m.longestOnes(new int[]{1, 1, 1, 1, 1}, 0)).isEqualTo(5);
+        assertThat(m.longestOnes(new int[]{1, 1, 1, 1, 1}, 100)).isEqualTo(5);
+    }
 }
