7135208fd1a276272e76a885ab54f3d6f0e9d36d
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2025-02-22 11:09:06 +0000

    https://leetcode.com/problems/dota2-senate

--- src/main/java/net/zkiss/leetcode/Dota2Senate.java
+++ src/main/java/net/zkiss/leetcode/Dota2Senate.java
@@ -1,37 +1,47 @@
 package net.zkiss.leetcode;
 
+import java.util.LinkedList;
+
 //https://leetcode.com/problems/dota2-senate
 public class Dota2Senate {
     public String predictPartyVictory(String senate) {
-        var ss = senate.toCharArray();
-        var balance = 0; // d <= 0 <= r
+        var elim = new LinkedList<Character>();
+        for (var c : senate.toCharArray()) {
+            elim.add(c);
+        }
+        var dVote = 0;
+        var rVote = 0;
         while (true) {
-            var rLive = 0;
             var dLive = 0;
-            for (int i = 0; i < ss.length; i++) {
-                if (ss[i] == 'R') {
-                    if (balance < 0) {
-                        ss[i] = 'X';
+            var rLive = 0;
+            var i = elim.iterator();
+            while (i.hasNext()) {
+                var c = i.next();
+                if (c == 'D') {
+                    if (rVote > 0) {
+                        rVote--;
+                        i.remove();
                     } else {
-                        rLive++;
-                    }
-                    balance++;
-                } else if (ss[i] == 'D') {
-                    if (0 < balance) {
-                        ss[i] = 'X';
-                    } else {
+                        dVote++;
                         dLive++;
                     }
-                    balance--;
+                } else { // R
+                    if (dVote > 0) {
+                        dVote--;
+                        i.remove();
+                    } else {
+                        rVote++;
+                        rLive++;
+                    }
                 }
+            }
+
+            if (dLive == 0) {
+                return "Radiant";
             }
             if (rLive == 0) {
                 return "Dire";
             }
-            if (dLive == 0) {
-                return "Radiant";
-            }
         }
     }
-
 }
