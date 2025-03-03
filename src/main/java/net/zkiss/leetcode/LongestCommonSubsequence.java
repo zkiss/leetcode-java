38790855abfa1f4f5ec1058781dfe0eebb48da39
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2025-03-03 20:48:37 +0000

    https://leetcode.com/problems/longest-common-subsequence

--- src/main/java/net/zkiss/leetcode/LongestCommonSubsequence.java
+++ src/main/java/net/zkiss/leetcode/LongestCommonSubsequence.java
@@ -4,20 +4,20 @@ package net.zkiss.leetcode;
 public class LongestCommonSubsequence {
     public int longestCommonSubsequence(String text1, String text2) {
         var results = new int[text1.length()][text2.length()];
-        for (int i = text1.length() - 1; 0 <= i; i--) {
+        for (var i = 0; i < text1.length(); i++) {
             var c1 = text1.charAt(i);
-            for (int j = text2.length() - 1; 0 <= j; j--) {
+            for (var j = 0; j < text2.length(); j++) {
                 var c2 = text2.charAt(j);
                 if (c1 == c2) {
-                    var tail = i == text1.length() - 1 || j == text2.length() - 1 ? 0 : results[i + 1][j + 1];
-                    results[i][j] = 1 + tail;
+                    var prefix = i == 0 || j == 0 ? 0 : results[i - 1][j - 1];
+                    results[i][j] = 1 + prefix;
                 } else {
-                    var t1Tail = i == text1.length() - 1 ? 0 : results[i + 1][j];
-                    var t2Tail = j == text2.length() - 1 ? 0 : results[i][j + 1];
-                    results[i][j] = Math.max(t1Tail, t2Tail);
+                    var t1Prefix = i == 0 ? 0 : results[i - 1][j];
+                    var t2Prefix = j == 0 ? 0 : results[i][j - 1];
+                    results[i][j] = Math.max(t1Prefix, t2Prefix);
                 }
             }
         }
-        return results[0][0];
+        return results[text1.length() - 1][text2.length() - 1];
     }
 }
