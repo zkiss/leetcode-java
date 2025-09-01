e0f7fa1b64a0c147bdc866537907f2571b2f96f9
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2025-09-01 21:48:31 +0100

    Update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.26.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
