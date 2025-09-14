8fb0babaa6ac4d05be345bf01555d9d99c3aff7e
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2025-09-14 10:31:36 +0100

    https://leetcode.com/problems/reverse-words-in-a-string

--- src/test/java/net/zkiss/leetcode/ReverseWordsInAStringTest.java
+++ src/test/java/net/zkiss/leetcode/ReverseWordsInAStringTest.java
@@ -12,4 +12,19 @@ class ReverseWordsInAStringTest {
     void example1() {
         assertThat(rw.reverseWords("the sky is blue")).isEqualTo("blue is sky the");
     }
+
+    @Test
+    void example2() {
+        assertThat(rw.reverseWords("  hello world  ")).isEqualTo("world hello");
+    }
+
+    @Test
+    void example3() {
+        assertThat(rw.reverseWords("a good   example")).isEqualTo("example good a");
+    }
+
+    @Test
+    void one_word() {
+        assertThat(rw.reverseWords("word")).isEqualTo("word");
+    }
 }
