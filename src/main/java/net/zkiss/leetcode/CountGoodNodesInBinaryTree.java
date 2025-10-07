0df8a3939a68a2fa81860e550aa9abf61934de32
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2025-10-07 20:19:49 +0100

    https://leetcode.com/problems/count-good-nodes-in-binary-tree

--- src/main/java/net/zkiss/leetcode/CountGoodNodesInBinaryTree.java
+++ src/main/java/net/zkiss/leetcode/CountGoodNodesInBinaryTree.java
@@ -2,24 +2,21 @@ package net.zkiss.leetcode;
 
 import net.zkiss.leetcode.common.TreeNode;
 
-//https://leetcode.com/problems/count-good-nodes-in-binary-tree
+// https://leetcode.com/problems/count-good-nodes-in-binary-tree
 public class CountGoodNodesInBinaryTree {
     public int goodNodes(TreeNode root) {
-        return goodNodes(root, root.val);
+        return goodNodes(root, Integer.MIN_VALUE);
     }
 
-    private int goodNodes(TreeNode root, int min) {
-        min = Math.max(min, root.val);
-        var ret = 0;
-        if (root.val >= min) {
-            ret++;
+    private int goodNodes(TreeNode root, int max) {
+        if (root == null) return 0;
+        var ans = 0;
+        if (root.val >= max) {
+            ans++;
         }
-        if (root.left != null) {
-            ret += goodNodes(root.left, min);
-        }
-        if (root.right != null) {
-            ret += goodNodes(root.right, min);
-        }
-        return ret;
+        max = Math.max(max, root.val);
+        ans += goodNodes(root.left, max);
+        ans += goodNodes(root.right, max);
+        return ans;
     }
 }
--- src/test/java/net/zkiss/leetcode/CountGoodNodesInBinaryTreeTest.java
+++ src/test/java/net/zkiss/leetcode/CountGoodNodesInBinaryTreeTest.java
@@ -1,8 +1,8 @@
 package net.zkiss.leetcode;
 
+import net.zkiss.leetcode.common.TreeNode;
 import org.junit.jupiter.api.Test;
 
-import static net.zkiss.leetcode.common.TreeNode.n;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CountGoodNodesInBinaryTreeTest {
@@ -11,28 +11,18 @@ class CountGoodNodesInBinaryTreeTest {
 
     @Test
     void example1() {
-        var root = n(3,
-                n(1,
-                        n(3),
-                        null),
-                n(4,
-                        n(1),
-                        n(5)));
+        var root = TreeNode.of(3, 1, 4, 3, null, 1, 5);
         assertThat(cgn.goodNodes(root)).isEqualTo(4);
     }
 
     @Test
     void example2() {
-        var root = n(3,
-                n(3,
-                        n(4),
-                        n(2)),
-                null);
+        var root = TreeNode.of(3, 3, null, 4, 2);
         assertThat(cgn.goodNodes(root)).isEqualTo(3);
     }
 
     @Test
     void example3() {
-        assertThat(cgn.goodNodes(n(1))).isEqualTo(1);
+        assertThat(cgn.goodNodes(TreeNode.of(1))).isEqualTo(1);
     }
 }
